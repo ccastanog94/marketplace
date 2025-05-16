b07daf67f4b8d9ada4574310df09e84a9f8d52c2
Claudia Alejandra Castaño <ccastanog94@gmail.com>
2025-05-15 19:32:39 -0500

    fix: importación de AuthController en api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::apiResource('products', ProductController::class);
