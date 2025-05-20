a66f0c5d96b5b05bf133437e508537521b3801e5
Claudia Alejandra Castaño <ccastanog94@gmail.com>
2025-05-19 20:20:47 -0500

    Blade productos

---
+++ resources/views/productos.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Listado de productos</h1>
+        <p>¡Esta es la página de productos!</p>
+    </div>
+@endsection
